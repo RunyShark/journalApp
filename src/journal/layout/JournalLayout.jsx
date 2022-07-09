97c79b3c705cdf75fd15fc467ae493ec39c8996b
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-08 21:37:47 -0500

    styleNavBar

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,8 +1,10 @@
 import { Box } from "@mui/material";
+import { NavBar } from "../../index";
 const drawerWidth = 240;
 export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: "flex" }}>
+      <NavBar drawerWidth={drawerWidth} />
       <Box component="main" sx={{ flexGrow: 1, p: 2 }}>
         {children}
       </Box>
