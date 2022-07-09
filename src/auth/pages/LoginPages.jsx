43483122000f3091f091c40a549f7776e67c897c
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-08 20:25:12 -0500

    styleLogin

--- src/auth/pages/LoginPages.jsx
+++ src/auth/pages/LoginPages.jsx
@@ -1,4 +1,6 @@
-import { Grid, Typography, TextField } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import { Google } from "@mui/icons-material";
+import { Grid, Typography, TextField, Button, Link } from "@mui/material";
 export const LoginPages = () => {
   return (
     <Grid
@@ -35,6 +37,24 @@ export const LoginPages = () => {
                 placeholder="Escribe tu contraseña"
                 fullWidth
               />
+            </Grid>
+            <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+              <Grid item xs={12} sm={6}>
+                <Button variant="contained" fullWidth>
+                  Login
+                </Button>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Button variant="contained" fullWidth>
+                  <Google />
+                  <Typography sx={{ ml: 1 }}>Google</Typography>
+                </Button>
+              </Grid>
+            </Grid>
+            <Grid container direction="row" justifyContent="end">
+              <Link component={RouterLink} color="inherit" to="/auth/register">
+                Crear una cuenta
+              </Link>
             </Grid>
           </Grid>
         </form>
