2471b07bb36bce256337371af11b50d1f95ca600
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-08 21:53:31 -0500

    styleSideBar

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,10 +1,11 @@
 import { Box } from "@mui/material";
-import { NavBar } from "../../index";
-const drawerWidth = 240;
+import { NavBar, SideBar } from "../../index";
+const drawerWidth = 300;
 export const JournalLayout = ({ children }) => {
   return (
     <Box sx={{ display: "flex" }}>
       <NavBar drawerWidth={drawerWidth} />
+      <SideBar drawerWidth={drawerWidth} />
       <Box component="main" sx={{ flexGrow: 1, p: 2 }}>
         {children}
       </Box>
---
+++ src/ui/components/SideBar.jsx
@@ -0,0 +1,58 @@
+import {
+  Box,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+  Typography,
+  Grid,
+} from "@mui/material";
+import { TurnedInNot } from "@mui/icons-material";
+
+export const SideBar = ({ drawerWidth = 240 }) => {
+  return (
+    <Box
+      component="nav"
+      sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+    >
+      <Drawer
+        variant="permanent"
+        open
+        sx={{
+          display: { sx: "block" },
+          "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
+        }}
+      >
+        <Toolbar>
+          <Typography variant="h6" noWrap component="div">
+            Dario
+          </Typography>
+        </Toolbar>
+        <Divider />
+        <List>
+          {["Enero", "Febrero", "Marzo", "Abrir"].map((text) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  <TurnedInNot />
+                </ListItemIcon>
+                <Grid container>
+                  <ListItemText primary={text} />
+                  <ListItemText
+                    secondary={
+                      "Exercitaion cillum irure elit consecteturn ipsum duis quis nisi tempor dolor"
+                    }
+                  />
+                </Grid>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </Box>
+  );
+};
