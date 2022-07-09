02df270a365d32d950dd18a961bdab4c773350ca
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-08 19:00:25 -0500

    login

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { JournalApp } from "./index";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <JournalApp />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <JournalApp />
+    </React.StrictMode>
+  </BrowserRouter>
 );
