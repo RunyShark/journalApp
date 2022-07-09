97afbd3cc3c0a92057f8c9688f485c9e3320fde6
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-08 19:15:27 -0500

    ConfingRoutes

--- src/journal/router/JournalRoutes.jsx
+++ src/journal/router/JournalRoutes.jsx
@@ -1,10 +1,11 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import { JournalPage } from "../../index";
 
 export const JournalRoutes = () => {
   return (
     <Routes>
-      <Route path="" element={<JournalPage />} />
+      <Route path="/" element={<JournalPage />} />
+      <Route path="/*" element={<Navigate to="/" />} />
     </Routes>
   );
 };
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
+import { AuthRoutes, JournalRoutes } from "../index";
 
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route />
+      <Route path="auth/*" element={<AuthRoutes />} />
 
-      <Route />
+      <Route path="/*" element={<JournalRoutes />} />
     </Routes>
   );
 };
