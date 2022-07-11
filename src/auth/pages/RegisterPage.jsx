9a05bbfc5e2952217ff069206c1fd46f713a3005
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-10 19:36:19 -0500

    RegistroFirebase

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,39 +1,57 @@
 import { Link as RouterLink } from "react-router-dom";
-import { AuthLayout } from "../../index";
+import { AuthLayout, useForms } from "../../index";
 import { Grid, Typography, TextField, Button, Link } from "@mui/material";
 export const RegisterPage = () => {
+  const { email, password, name, onInputChague, onResetFrom } = useForms({
+    name: "",
+    email: "correo@correo",
+    password: 1234,
+  });
+  const onSubmit = (event) => {
+    event.preventDefault();
+    onResetFrom();
+  };
   return (
     <AuthLayout title="Crear cuenta">
-      <form>
+      <form onSubmit={onSubmit}>
         <Grid container>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
               label="Nombre completo"
               type="text"
+              name="name"
+              value={name}
               placeholder="Escribe tu nombre"
               fullWidth
+              onChange={onInputChague}
             />
           </Grid>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
               label="Correo"
               type="email"
+              name="email"
+              value={email}
               placeholder="Escribe tu corre"
               fullWidth
+              onChange={onInputChague}
             />
           </Grid>
           <Grid item xs={12} sx={{ mt: 2 }}>
             <TextField
               label="Contraseña"
               type="password"
+              name="password"
+              value={password}
               placeholder="Escribe tu contraseña"
               fullWidth
+              onChange={onInputChague}
             />
           </Grid>
 
           <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
             <Grid item xs={12}>
-              <Button variant="contained" fullWidth>
+              <Button variant="contained" fullWidth type="submit">
                 Registro
               </Button>
             </Grid>
