c1605a509f51a924867d8fbafd922fcc8b8b785e
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-10 23:35:54 -0500

    check

---
+++ src/ui/components/ChekingAuth.jsx
@@ -0,0 +1,26 @@
+import { Grid } from "@mui/material";
+
+export const ChekingAuth = () => {
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ minHeight: "100vh", backgroundColor: "primary.main", padding: 4 }}
+    >
+      <Grid
+        item
+        className="box-shadow"
+        xs={3}
+        sx={{
+          width: { md: 450 },
+          backgroundColor: "white",
+          padding: 3,
+          borderRadius: 2,
+        }}
+      ></Grid>
+    </Grid>
+  );
+};
