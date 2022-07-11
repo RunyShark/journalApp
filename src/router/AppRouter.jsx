b38719306c70f87e2515c9b8fe5dd5b5bb6801ef
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-11 12:06:30 -0500

    Login

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,10 +1,11 @@
-import { useSelector, useEffect } from "react-redux";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { onAuthStateChanged } from "firebase/auth";
 import { Route, Routes } from "react-router-dom";
 import { AuthRoutes, JournalRoutes, ChekingAuth, FirebaseAuth } from "../index";
-import { async } from "@firebase/util";
 
 export const AppRouter = () => {
+  const dispatch = useDispatch();
   useEffect(() => {
     onAuthStateChanged(FirebaseAuth, async (user) => {
       console.log(user);
